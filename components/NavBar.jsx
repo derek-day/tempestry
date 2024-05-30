22878f84c7036f88bc7ba72deaa57c2dee2d177e
Derek Day <derek.andrew.day@gmail.com>
2024-05-30 02:35:46 -0400

    a

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -29,7 +29,6 @@ const NavBar = () => {
       <div className="nav-container" data-testid="navbar">
         <Navbar expand="md">
           <Container>
-            <NavbarBrand className="logo" />
             <NavbarToggler onClick={toggle} data-testid="navbar-toggle" />
             <Collapse isOpen={isOpen} navbar>
               <Nav className="mr-auto" navbar data-testid="navbar-items">
